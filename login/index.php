11a57274b24131f1e7be1b0466950448016aefce
Alejandro Gonzalez <alexgve7@gmail.com>
2019-04-22 14:07:56 -0600

    CSS and Icons and Login Folders and Account

--- login/index.php
+++ login/index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-    <link rel="stylesheet" href="../global/css/materialize.min.css">
-    <link rel="stylesheet" href="../global/css/material-icons.css">
+    <title>Document</title>
 </head>
 <body>
     
